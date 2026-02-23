1476b120bd0036b46fc079b296ab5be433b0b43b
Ashikur Rahman <shopnilashik4@gmail.com>
2026-02-23 07:14:31 +0600

    fix: store.js casing and orderApi path

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import authReducer from "./features/auth/authSlice";
 import productApi from './features/products/productsApi';
 import reviewsApi from './features/reviews/reviewsApi';
 import cartReducer from './features/cart/CartSlice'
-import orderApi from './features/orders/orderapi';
+import orderApi from './features/orders/orderApi'; // Ekhane 'A' boro hater korun
 import statsApi from './features/stats/statsApi';
 
 export const store = configureStore({
@@ -14,9 +14,15 @@ export const store = configureStore({
         [productApi.reducerPath]: productApi.reducer,
         [reviewsApi.reducerPath]: reviewsApi.reducer,
          cart: cartReducer,
-         [orderApi.reducerPath]: orderApi.reducer,
+         [orderApi.reducerPath]: orderApi.reducer, // Ekhane 'A' boro hater
          [statsApi.reducerPath]: statsApi.reducer
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, productApi.middleware, reviewsApi.middleware, orderApi.middleware, statsApi.middleware)
+    getDefaultMiddleware().concat(
+        authApi.middleware, 
+        productApi.middleware, 
+        reviewsApi.middleware, 
+        orderApi.middleware, // Ekhane 'A' boro hater
+        statsApi.middleware
+    )
 })
